7fac247ec549667e7ad02e0a3c6eadc3935f4aaf
june545 <545329942@qq.com>
2016-06-23 16:36:28 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/.name
    #	.idea/gradle.xml
    #	.idea/misc.xml
    #	.idea/modules.xml
    #	.idea/vcs.xml
    #	toolbar_demo/src/main/java/com/example/hp/myapplication/CollapsingToolbarLayoutActivity.java
    #	toolbar_demo/src/main/java/com/example/hp/myapplication/MainActivity.java
    #	toolbar_demo/src/main/res/layout/activity_main.xml
    #	toolbar_demo/src/main/res/layout/app_bar_main.xml

--- animator/src/main/java/com/example/animator/AnimActivity.java
+++ animator/src/main/java/com/example/animator/AnimActivity.java
@@ -1,5 +1,6 @@
 package com.example.animator;
 
+import android.animation.ObjectAnimator;
 import android.animation.PointFEvaluator;
 import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
@@ -10,6 +11,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.animation.ValueAnimatorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -18,7 +21,7 @@ import android.widget.LinearLayout;
  */
 public class AnimActivity extends AppCompatActivity {
 	LinearLayout rootView;
-	ImageView img;
+	ImageView    img;
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,24 +31,25 @@ public class AnimActivity extends AppCompatActivity {
 		rootView = (LinearLayout) findViewById(R.id.rootview);
 		img = (ImageView) findViewById(R.id.imgView);
 
-		img.setOnClickListener(new View.OnClickListener() {
+		rootView.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				m();
+				m4();
 			}
 		});
 	}
 
 	@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 	void m() {
-		ValueAnimator valueAnimator = ValueAnimator.ofInt(1, 100);
+		// 属性动画，根据view属性值变化，自己手动刷新view位置、形态
+		ValueAnimator valueAnimator = ValueAnimator.ofInt(1, 500);
 		valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 			@Override
 			public void onAnimationUpdate(ValueAnimator animation) {
 				int value = (int) animation.getAnimatedValue();
 
-				int left = value - img.getWidth() / 2;
-				int top = value - img.getHeight() / 2;
+				int left = value + img.getWidth() / 2;
+				int top = value + img.getHeight() / 2;
 				int width = left + img.getWidth();
 				int height = top + img.getHeight();
 				img.layout(left, top, width, height);
@@ -53,5 +57,45 @@ public class AnimActivity extends AppCompatActivity {
 		});
 		valueAnimator.setDuration(5000);
 		valueAnimator.start();
+	}
+
+	/**
+	 * 复合动画
+	 */
+	void m2() {
+
+		ObjectAnimator animator = ObjectAnimator.ofFloat(img, "translationX", 0.0f, 350.0f, 0.0f);
+		animator.setDuration(2500).start();
+		ObjectAnimator animator1 = ObjectAnimator.ofFloat(img, "scaleX", 1.0f, 1.5f, 1.0f);
+		animator1.setDuration(2000).start();
+
+		ObjectAnimator animator3 = ObjectAnimator.ofFloat(img, "rotationX", 0.0f, 90.0f, 0.0F);
+		animator3.setDuration(2000).start();
+
+		ObjectAnimator animator4 = ObjectAnimator.ofFloat(img, "alpha", 1.0f, 0.3f, 1.0F);
+		animator4.setDuration(2000).start();
+	}
+
+	void m3() {
+		ObjectAnimator animator = ObjectAnimator.ofFloat(img, "alpha", 1.0f, 0.3f, 1.0F);
+		animator.setDuration(2000);//动画时间
+		animator.setInterpolator(new BounceInterpolator());//动画插值
+		animator.setRepeatCount(-1);//设置动画重复次数
+		animator.setRepeatMode(ValueAnimator.RESTART);//动画重复模式
+		animator.setStartDelay(1000);//动画延时执行
+		animator.start();
+	}
+
+	/**
+	 * 旋转
+	 */
+	void m4(){
+		ObjectAnimator animator = ObjectAnimator.ofFloat(img, "rotation", 0, 45, 90, 135, 180, 225, 270, 315, 360);
+		animator.setDuration(2000);//动画时间
+		animator.setInterpolator(new LinearInterpolator());//动画插值
+		animator.setRepeatCount(-1);//设置动画重复次数
+		animator.setRepeatMode(ValueAnimator.RESTART);//动画重复模式
+		animator.setStartDelay(1000);//动画延时执行
+		animator.start();
 	}
 }
